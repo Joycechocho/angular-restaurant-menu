60dc01f01285620dcf537a92e02e37057df244b9
Joyce Cho <joyce.cho@colorado.edu>
2018-04-15 20:51:22 -0600

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material.module';
+import { MatGridListModule } from '@angular/material/grid-list'
 
-
+import 'hammerjs';
 import { AppComponent } from './app.component';
-
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './menu/dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    MatGridListModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DISHES} from '../shared/dishes';
+import { DishService } from '../services/dish.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  dishes = DISHES;
+  selectedDish: Dish;
+  onSelect(dish: Dish) {
+    this.selectedDish = dish;
+  }
+  constructor(private dishService: DishService) { }
+  ngOnInit() {
+    this.dishes = this.dishService.getDishes();
+  }
+
+}
